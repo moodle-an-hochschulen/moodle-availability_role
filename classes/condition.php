deaed0d8d4a45f0e4c69647672fdc3f2eff7ecea
Alexander Bias <alexander.bias@uni-ulm.de>
2016-01-28 15:52:46 +0100

    First final version, delivered by Synergy Learning

---
+++ classes/condition.php
@@ -0,0 +1,131 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Condition main class.
+ *
+ * @package availability_role
+ * @copyright Bence Laky, Synergy Learning UK <b.laky@intrallect.com>
+ * on behalf of Alexander Bias, University of Ulm <alexander.bias@uni-ulm.de>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace availability_role;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Condition main class.
+ *
+ * @package availability_role
+ * @copyright Bence Laky <b.laky@intrallect.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class condition extends \core_availability\condition {
+    /** @var int ID of role that this condition requires */
+    protected $roleid = 0;
+
+    /**
+     * Constructor.
+     *
+     * @param \stdClass $structure Data structure from JSON decode
+     * @throws \coding_exception If invalid data structure.
+     */
+    public function __construct($structure) {
+        // Get role id.
+        if (isset($structure->id) && is_int($structure->id)) {
+                $this->roleid = $structure->id;
+        } else {
+            throw new \coding_exception('Invalid ->id for role condition');
+        }
+    }
+
+    public function save() {
+        $result = (object)array('type' => 'role');
+        if ($this->roleid) {
+            $result->id = $this->roleid;
+        } else {
+            $result->activity = true;
+        }
+        return $result;
+    }
+
+    public function include_after_restore($restoreid, $courseid, \base_logger $logger,
+            $name, \base_task $task) {
+                return !$this->roleid || $task->get_setting_value('roles');
+    }
+
+    public function update_after_restore($restoreid, $courseid, \base_logger $logger, $name) {
+        global $DB;
+        if (!$this->roleid) {
+            // If using 'same as activity' option, no need to change it.
+            return false;
+        }
+        $rec = \restore_dbops::get_backup_ids_record($restoreid, 'role', $this->roleid);
+        if (!$rec || !$rec->newitemid) {
+            // If we are on the same course (e.g. duplicate) then we can just
+            // use the existing one.
+            if ($DB->record_exists('roles',
+                    array('id' => $this->roleid, 'courseid' => $courseid))) {
+                        return false;
+                    }
+                    // Otherwise it's a warning.
+                    $this->roleid = -1;
+                    $logger->process('Restored item (' . $name .
+                            ') has availability condition on grouping that was not restored',
+                            \backup::LOG_WARNING);
+        } else {
+            $this->roleid = (int)$rec->newitemid;
+        }
+
+        return true;
+    }
+
+    public function is_available($not, \core_availability\info $info, $grabthelot, $userid) {
+        $context = \context_course::instance($info->get_course()->id);
+        $allow = false;
+
+        foreach(get_user_roles($context, $userid) as $role) {
+            if ($role->roleid == $this->roleid) {
+                $allow = true;
+                break;
+            }
+        }
+
+        if ($not) {
+            $allow = !$allow;
+        }
+        return $allow;
+    }
+
+    public function get_description($full, $not, \core_availability\info $info) {
+        global $DB;
+        $context = \context_course::instance($info->get_course()->id);
+        $role = $DB->get_record('role', array('id' => $this->roleid));
+        if (!$role) {
+            return get_string('missing', 'availability_role');
+        } else {
+            $name = role_get_name($role, $context);
+            return get_string($not ? 'requires_notrole' : 'requires_role', 'availability_role',
+                    $name);
+        }
+    }
+
+    public function get_debug_string() {
+        return "Role id: $this->roleid";
+    }
+
+}
---
+++ lang/en/availability_role.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language strings.
+ *
+ * @package availability_role
+ * @copyright Bence Laky, Synergy Learning UK <b.laky@intrallect.com>
+ * on behalf of Alexander Bias, University of Ulm <alexander.bias@uni-ulm.de>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['description'] = 'Allow only users with a specified role.';
+$string['error_selectrole'] = 'You must select a role.';
+$string['missing'] = '(Missing role)';
+$string['title'] = 'Role';
+$string['pluginname'] = 'Restriction by course role';
+$string['requires_role'] = 'You are a(n) <strong>{$a}</strong>.';
+$string['requires_notrole'] = 'You are not a(n) <strong>{$a}</strong>.';
