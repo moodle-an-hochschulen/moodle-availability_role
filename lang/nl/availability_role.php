698e6328d1f449f8e76925c0cd67c4b1923109cc
Ted van den Brink <ted.vandenbrink@brightalley.nl>
2017-04-18 13:31:11 +0200

    Added Dutch language

---
+++ lang/nl/availability_role.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Availability role - Language pack
+ *
+ * @package    availability_role
+ * @copyright  2017 Ted van den Brink, Bright Alley <ted.vandenbrink@brightalley.nl>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['description'] = 'Beperk toegang door middel van een specifieke cursus rol.';
+$string['error_selectrole'] = 'U moet een rol selecteren';
+$string['missing'] = '[Rol ontbreekt]';
+$string['title'] = 'Rol';
+$string['pluginname'] = 'Beperk toegang door middel van rol';
+$string['requires_role'] = 'Je hebt de rol <em>{$a}</em>';
+$string['requires_notrole'] = 'Je hebt de rol <em>{$a}</em> niet';
