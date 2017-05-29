92fc25be36314d5b7276e4f5177a7f4d53559305
Alexander Bias <alexander.bias@uni-ulm.de>
2017-05-29 15:01:07 +0200

    Release v3.2-r4

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'availability_role';
-$plugin->version = 2017050500;
-$plugin->release = 'v3.2-r3';
+$plugin->version = 2017052900;
+$plugin->release = 'v3.2-r4';
 $plugin->requires = 2016120500;
 $plugin->maturity = MATURITY_STABLE;
