cdce0ce2a7184003754e31e256311f127116b142
Kathrin Osswald <kathrin.osswald@uni-ulm.de>
2017-12-04 13:13:20 +0100

    Check compatibility for Moodle 3.3, no functionality change.

--- version.php
+++ version.php
@@ -28,5 +28,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'availability_role';
 $plugin->version = 2017063000;
 $plugin->release = 'v3.2-r5';
-$plugin->requires = 2016120500;
+$plugin->requires = 2017051500;
 $plugin->maturity = MATURITY_STABLE;
