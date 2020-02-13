5a7978bfe0ff51323212509db9744f68bde22790
Kathrin Osswald <kathrin.osswald@uni-ulm.de>
2020-02-13 08:31:45 +0100

    Release v3.8-r1

--- CHANGES.md
+++ CHANGES.md
@@ -4,7 +4,7 @@ moodle-availability_role
 Changes
 -------
 
-### Unreleased
+### v3.8-r1
 
 * 2020-02-13 - Prepare compatibility for Moodle 3.8.
 * 2019-07-04 - Optimized behat steps.
--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'availability_role';
-$plugin->version = 2019061400;
-$plugin->release = 'v3.7-r1';
+$plugin->version = 2020021300;
+$plugin->release = 'v3.8-r1';
 $plugin->requires = 2019111800;
 $plugin->maturity = MATURITY_STABLE;
