4957aa9c3fd2b5bcb7460bbaf0988eba78af1a73
Kathrin Osswald <kathrin.osswald@uni-ulm.de>
2020-07-16 08:09:54 +0200

    Prepare compatibility for Moodle 3.9.

--- CHANGES.md
+++ CHANGES.md
@@ -4,6 +4,10 @@ moodle-availability_role
 Changes
 -------
 
+### Unreleased
+
+* 2020-07-16 - Prepare compatibility for Moodle 3.9.
+
 ### v3.8-r1
 
 * 2020-02-13 - Prepare compatibility for Moodle 3.8.
--- version.php
+++ version.php
@@ -28,5 +28,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'availability_role';
 $plugin->version = 2020021300;
 $plugin->release = 'v3.8-r1';
-$plugin->requires = 2019111800;
+$plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_STABLE;
