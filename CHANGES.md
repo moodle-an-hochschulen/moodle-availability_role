37d10ac6cd79fec511b4a50bf68934c00ed23ffc
Alexander Bias <bias@alexanderbias.de>
2025-06-01 10:29:28 +0200

    Release v4.5-r2

--- CHANGES.md
+++ CHANGES.md
@@ -4,7 +4,7 @@ moodle-availability_role
 Changes
 -------
 
-### Unreleased
+### v4.5-r2
 
 * 2025-05-18 - Feature: Allow the admin to configure which roles can be used, resolves #18
 
--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'availability_role';
-$plugin->version = 2024100701;
-$plugin->release = 'v4.5-r1';
+$plugin->version = 2024100702;
+$plugin->release = 'v4.5-r2';
 $plugin->requires = 2024100700;
 $plugin->supported = [405, 405];
 $plugin->maturity = MATURITY_STABLE;
