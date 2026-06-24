1681d7aa4953659099570ab1b01c7581b4917d0a
Alexander Bias <bias@alexanderbias.de>
2026-06-24 08:15:29 +0200

    Release v4.5-r5

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'availability_role';
-$plugin->version = 2024100706;
-$plugin->release = 'v4.5-r4';
+$plugin->version = 2024100707;
+$plugin->release = 'v4.5-r5';
 $plugin->requires = 2024100700;
 $plugin->supported = [405, 405];
 $plugin->maturity = MATURITY_STABLE;
